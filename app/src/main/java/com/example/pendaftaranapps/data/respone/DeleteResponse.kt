b129568acac2b0327fea30cd5248bbbcbd98604a
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-20 21:29:16 +0700

    Menambahkan DeleteResponse

---
+++ app/src/main/java/com/example/pendaftaranapps/data/respone/DeleteResponse.kt
@@ -0,0 +1,9 @@
+package com.example.pendaftaranapps.data.respone
+
+import com.google.gson.annotations.SerializedName
+
+data class DeleteResponse(
+
+	@field:SerializedName("message")
+	val message: String
+)
